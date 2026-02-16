8e9353b4c1f46336a3d7fee25429941aeab02fa8
Subhash <bashamalla.subhash@gmail.com>
2026-02-16 12:14:38 +0530

    Create order.py

---
+++ app/models/order.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from app.core.database import Base
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
