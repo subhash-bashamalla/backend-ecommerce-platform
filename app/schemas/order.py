325de8ad73e14f46e28855455a8ef80c176f525f
Subhash <bashamalla.subhash@gmail.com>
2026-02-16 13:21:14 +0530

    Update order.py

--- app/schemas/order.py
+++ app/schemas/order.py
@@ -1,15 +1,21 @@
 from pydantic import BaseModel
+from typing import Optional
+from app.schemas.product import ProductResponse
 
-class OrderCreate(BaseModel):
+
+class OrderBase(BaseModel):
     product_id: int
     quantity: int
+
+
+class OrderCreate(OrderBase):
+    pass
+
 
 class OrderResponse(BaseModel):
     id: int
-    user_id: int
-    product_id: int
     quantity: int
-    price_at_purchase: float
+    product: ProductResponse
 
     class Config:
-        from_attributes = True
+        orm_mode = True
