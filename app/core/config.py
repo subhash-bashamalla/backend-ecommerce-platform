3d8964c37e0d07239dfcd4660b5b80f3873194ff
Subhash <bashamalla.subhash@gmail.com>
2026-02-16 12:58:55 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,22 @@
 import os
-from dotenv import load_dotenv
+from pydantic import BaseSettings
 
-load_dotenv()
 
-class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 60
-    REDIS_URL = os.getenv("REDIS_URL")
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "E-Commerce Backend"
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:postgres@localhost:5432/ecommerce"
+    )
+
+    REDIS_HOST: str = os.getenv("REDIS_HOST", "localhost")
+    REDIS_PORT: int = int(os.getenv("REDIS_PORT", 6379))
+    REDIS_DB: int = int(os.getenv("REDIS_DB", 0))
+
 
 settings = Settings()
