e1728676357fd16186fafa3a64da395a2a65c32d
Subhash <bashamalla.subhash@gmail.com>
2026-02-16 13:01:30 +0530

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -2,14 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
-DATABASE_URL = settings.DATABASE_URL
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-    if "sqlite" in DATABASE_URL
-    else {}
-)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
